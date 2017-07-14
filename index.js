9a03b7a0aaa9677165107ca88b6b5d0028845dea
Vojtěch Lank <vojta@lankovi.cz>
2017-07-15 01:19:53 +0200

    oprava fomratovani datumu z DB

--- index.js
+++ index.js
@@ -76,7 +76,8 @@ var mysql = new Client({
         host     : process.env.JAWSDB_HOST,
 		user     : process.env.JAWSDB_USERNAME,
 		password : process.env.JAWSDB_PASSWORD,
-		database : process.env.JAWSDB_DBNAME
+		database : process.env.JAWSDB_DBNAME,
+		dateStrings: 'date'
     }
 });
 
